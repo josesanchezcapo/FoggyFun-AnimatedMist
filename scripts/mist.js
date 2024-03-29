197ec1d50c2a8bc8d64454bc15952c656f2d37da
Jose Sanchez-Capo <4494590+josesanchezcapo@users.noreply.github.com>
2024-03-28 21:21:08 -0600

    documenting code

--- scripts/mist.js
+++ scripts/mist.js
@@ -1,15 +1,18 @@
+// Function to create fog elements
 function createFog() {
-    const fogLayer = document.querySelector('.fog-layer');
+    const fogLayer = document.querySelector('.fog-layer'); // Select the fog layer element
     const numFogs = 30; // Adjust the number of fog elements
   
+    // Loop to create fog elements
     for (let i = 0; i < numFogs; i++) {
-      const fog = document.createElement('div');
-      fog.classList.add('fog');
-      fog.style.left = `${Math.random() * 100}%`;
-      fog.style.animationDuration = `${Math.random() * 20 + 5}s`; // Adjust fog animation duration
-      fogLayer.appendChild(fog);
+      const fog = document.createElement('div'); // Create a div element for fog
+      fog.classList.add('fog'); // Add 'fog' class to the fog element
+      fog.style.left = `${Math.random() * 100}%`; // Set a random horizontal position
+      fog.style.animationDuration = `${Math.random() * 20 + 5}s`; // Set a random animation duration for fog movement
+      fogLayer.appendChild(fog); // Append fog element to fog layer
     }
   }
   
-  createFog();
+  createFog(); // Call the createFog function to generate fog elements
+  
   
